7c296a6b3d7fd737f6e7466546fd028650eaf461
firuzaa <46763531+firuzaa@users.noreply.github.com>
2019-02-02 19:55:06 -0500

    deleted previous user story 7 test commit

--- src/test/java/com/cybertek/tests/UserStory_7_Test.java
+++ src/test/java/com/cybertek/tests/UserStory_7_Test.java
@@ -6,24 +6,7 @@ import com.cybertek.utilities.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class UserStory_7_Test extends TestBase {
+public class UserStory_7_Test {
 
-
-
-    public UserStory_7 userStory_1;
-    UserStory_7 userStory7;
-
-    @Test
-    public void ShouldDisplay() {
-
-        userStory7 = new UserStory_7();
-
-        if (userStory7.customer1.isDisplayed()) {
-            userStory7.customer1.click();
-        }
-        Assert.assertTrue(userStory7.PrintBar.isDisplayed());
-        Assert.assertTrue(userStory7.ActionBar.isDisplayed());
-
-    }
 
 }
