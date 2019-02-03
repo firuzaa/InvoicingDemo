66dcc4216dc7262981e9b87c22bf21fa79871ceb
aa420 <alinura514@gmail.com>
2019-02-03 17:02:54 -0500

    added first user story third @Test

--- src/test/java/com/cybertek/pages/UserStory_3.java
+++ src/test/java/com/cybertek/pages/UserStory_3.java
@@ -1,4 +1,19 @@
 package com.cybertek.pages;
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class UserStory_3 {
+    public UserStory_3() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy(xpath = "//*[@id=\"oe_main_menu_navbar\"]/div[2]/ul[1]/li[8]/a/span")
+    public WebElement invoiceButton;
+
+    @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[8]/ul[1]/li[2]/a/span")
+    public WebElement masterDataButton;
+
+
+
 }
--- src/test/java/com/cybertek/tests/UserStory_3_Test.java
+++ src/test/java/com/cybertek/tests/UserStory_3_Test.java
@@ -1,4 +1,17 @@
 package com.cybertek.tests;
 
-public class UserStory_3_Test {
+import com.cybertek.pages.UserStory_3;
+import com.cybertek.utilities.TestBase;
+import org.testng.annotations.Test;
+
+public class UserStory_3_Test extends TestBase {
+    public UserStory_3 userStory_3;
+
+    @Test
+    public void masterData() {
+        userStory_3 = new UserStory_3();
+
+        userStory_3.invoiceButton.click();
+        userStory_3.masterDataButton.click();
+    }
 }
