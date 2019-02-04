1684d958283c2a676ba47531c90b5867358b107c
“Kuddus <ikuddus@gmail.com>
2019-02-04 14:08:50 -0500

    Added @Test for userStory_2

--- src/test/java/com/cybertek/pages/UserStory_2.java
+++ src/test/java/com/cybertek/pages/UserStory_2.java
@@ -1,4 +1,16 @@
 package com.cybertek.pages;
 
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class UserStory_2 {
+
+    public UserStory_2() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div/button[1]")
+    public WebElement createBtn;
 }
